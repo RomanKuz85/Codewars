122eed5d28fb769cb488e6613f064493308c2528
RomanKuz85 <roma.kuzmitskij@gmail.com>
2020-11-19 20:51:27 -0500

    added solution: Complete The Pattern #4

---
+++ 7 kyu/Complete The Pattern #4.js
@@ -0,0 +1,20 @@
+/*
+You have to write a function pattern which creates the following pattern upto n number of rows.
+If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+##Examples:
+pattern(4):
+1234
+234
+34
+4
+ */
+function pattern(n){
+    var output="";
+    for (let i = 1; i <= n; i++){
+        for (let j = i; j <= n; j++){
+            output += j;
+        }
+        output = output + '\n';
+    }
+    return output.slice(0, -1);
+}
